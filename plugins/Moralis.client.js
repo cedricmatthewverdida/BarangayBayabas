4ffce599ec80d6a51a5230270c4ed681fe0e721c
Cedric UwewU <70793428+cedricmatthewverdida@users.noreply.github.com>
2022-07-10 19:53:45 +0800

    Update Moralis.client.js

--- plugins/Moralis.client.js
+++ plugins/Moralis.client.js
@@ -2,6 +2,6 @@ import {Moralis} from 'moralis';
 
 
 Moralis.start({ 
-    serverUrl: "https://8wkwlohu8u17.usemoralis.com:2053/server", 
-    appId: "6CJ1wgYZQ87DmYvCr898C2rdhVpug4LBcZlr4e5s" 
+    serverUrl: "https://o5sg2brbqqqx.usemoralis.com:2053/server", 
+    appId: "PBPrHvDlkwtsXThmxp42YtYstUOr4Hi0m95XXh1o" 
 });
